5da0f0b345215cf2603e242707bf62394a22fb22
hlieb-kotiun <gleb.kotiun@gmail.com>
2026-08-07 14:34:50 +0200

    add refresh session

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,8 @@ import { User } from '../models/user.js';
 import bcrypt from 'bcrypt';
 // import { create } from 'handlebars';
 import jwt from 'jsonwebtoken';
+import { Session } from '../models/session.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -15,6 +17,10 @@ export const registerUser = async (req, res) => {
     email,
     password: hashedPassword,
   });
+
+  const newSession = await createSession(newUser._id);
+
+  setSessionCookies(res, newSession);
 
   res.status(201).json(newUser);
 };
@@ -36,5 +42,43 @@ export const loginUser = async (req, res) => {
     { expiresIn: '24h' },
   );
 
+  await Session.deleteOne({ userId: user._id });
+  const newSession = await createSession(user._id);
+  setSessionCookies(res, newSession);
+  res.status(200).json(user);
+
   res.status(200).json({ token, user });
+};
+
+export const refreshUserSession = async (req, res) => {
+  const { sessionId, refreshToken } = req.cookies;
+
+  console.log(sessionId, refreshToken);
+
+  const session = await Session.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+  console.log(session);
+
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const isRefreshTokenExpired = session.refreshTokenValidUntil < new Date();
+
+  if (isRefreshTokenExpired) {
+    await session.deleteOne();
+    res.clearCookie('sessionId');
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+    throw createHttpError(401, 'Session token expired');
+  }
+
+  await Session.deleteOne({ _id: session._id });
+  const newSession = await createSession(session.userId);
+  setSessionCookies(res, newSession);
+  res.status(200).json({
+    message: 'Session refreshed',
+  });
 };
