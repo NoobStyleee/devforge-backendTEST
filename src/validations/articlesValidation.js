819bda5fbd73e9cc15dc544e0741b2fe4a2c87d2
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-08-08 11:14:03 -0400

    fix: validate article id with mongoose

--- src/validations/articlesValidation.js
+++ src/validations/articlesValidation.js
@@ -1,9 +1,16 @@
 import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+
+const objectIdValidator = (value, helpers) => {
+  if (!isValidObjectId(value)) {
+    return helpers.error('any.invalid');
+  }
+
+  return value;
+};
 
 export const getArticleByIdSchema = {
   [Segments.PARAMS]: Joi.object({
-    id: Joi.string()
-      .pattern(/^[0-9a-fA-F]{24}$/)
-      .required(),
+    id: Joi.string().custom(objectIdValidator).required(),
   }),
 };
