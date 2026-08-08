7fd82a29e3570a9d97d9f8fd43f59de88e369d0f
IrynaDiatel <kastyrinaira@gmail.com>
2026-08-08 19:45:54 +0300

    fix: handle article endpoint errors

--- src/controllers/articlesController.js
+++ src/controllers/articlesController.js
@@ -1,9 +1,14 @@
 import { Article } from '../models/article.js';
+import createHttpError from 'http-errors';
 
 export const getArticlesByAuthorController = async (req, res) => {
   const { authorId } = req.params;
 
   const articles = await Article.find({ authorId });
 
+
+  if (!articles.length) {
+    throw createHttpError(404, 'Articles not found');
+  }
   res.status(200).json(articles);
 };
