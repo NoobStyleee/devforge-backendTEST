34dfb348e3f2f3dc4ff126a1fe9a0f81e8dce943
VolodymyrB <volodymyrbedryk@gmail.com>
2026-08-08 20:11:52 +0200

    added username

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,19 +1,19 @@
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import bcrypt from 'bcrypt';
-// import { create } from 'handlebars';
 
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 
 export const registerUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { username, email, password } = req.body;
   const userWithSameEmail = await User.findOne({ email });
   if (userWithSameEmail) {
     throw createHttpError(409, 'Email in use');
   }
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({
+    username,
     email,
     password: hashedPassword,
   });
