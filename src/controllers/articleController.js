272822f61f77b48aeb4804eaa7c8f4a385018ee5
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-08-10 12:54:05 +0300

    fixed createArticles

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -13,44 +13,4 @@ export const createArticle = async (req, res) => {
 
     await article.save();
     res.status(201).json(article);     
-};
-
-export const getArticles = async (req, res) => {
-  
-    const articles = await Article.find();
-    res.status(200).json(articles);
-  
-    res.status(500).json({ message: 'Server error'});
-  
-};
-
-export const getArticleById = async (req, res) => {
-  
-    const article = await Article.findById(req.params.id);   
-    res.status(200).json(article);
- 
-    res.status(500).json({ message: 'Server error'});
-  
-};
-
-export const updateArticle = async (req, res) => {
-
-    const article = await Article.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );   
-    res.status(200).json(article);
- 
-    res.status(500).json({ message: 'Server error'});
-  
-};
-
-export const deleteArticle = async (req, res) => {
-  
-    await Article.findByIdAndDelete(req.params.id);
-    res.status(200).json({ message: 'Article deleted successfully' });
-  
-    res.status(500).json({ message: 'Server error'});
-  
-};
+};        
--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createArticle, getArticles, getArticleById, updateArticle, deleteArticle } from '../controllers/articleController.js';
+import { createArticle, } from '../controllers/articleController.js';
 import { authenticate } from '../middleware/authenticate.js';
 import multer from 'multer';
 
@@ -7,18 +7,6 @@ const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
 // Створити статтю
-router.post('/articles', authenticate, upload.single('image'), createArticle);
-
-// Отримати всі статті
-router.get('/articles', authenticate, getArticles);
-
-// Отримати одну статтю
-router.get('/articles/:id', authenticate, getArticleById);
-
-// Оновити статтю
-router.patch('/articles/:id', authenticate, updateArticle);
-
-// Видалити статтю
-router.delete('/articles/:id', authenticate, deleteArticle);
+router.post('/articles', authenticate, upload.single('image'), createArticle);    
 
 export default router;
