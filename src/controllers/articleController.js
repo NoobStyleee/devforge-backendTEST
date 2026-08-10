34e98d0dedc6216bd5fb95175fb56a309efcb21c
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-08-10 10:13:10 +0300

    delete createArticle schema

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -1,10 +1,10 @@
-import { CreateArticle } from '../models/createArticle.js';
+import { Article } from "../models/Article.js";
 
 export const createArticle = async (req, res) => {
 
     const { title, content } = req.body;   
 
-    const article = new CreateArticle({
+    const article = new Article({
       title,
       content,
       image: req.file ? req.file.path : null,
@@ -17,7 +17,7 @@ export const createArticle = async (req, res) => {
 
 export const getArticles = async (req, res) => {
   
-    const articles = await CreateArticle.find();
+    const articles = await Article.find();
     res.status(200).json(articles);
   
     res.status(500).json({ message: 'Server error'});
@@ -26,7 +26,7 @@ export const getArticles = async (req, res) => {
 
 export const getArticleById = async (req, res) => {
   
-    const article = await CreateArticle.findById(req.params.id);   
+    const article = await Article.findById(req.params.id);   
     res.status(200).json(article);
  
     res.status(500).json({ message: 'Server error'});
@@ -35,7 +35,7 @@ export const getArticleById = async (req, res) => {
 
 export const updateArticle = async (req, res) => {
 
-    const article = await CreateArticle.findByIdAndUpdate(
+    const article = await Article.findByIdAndUpdate(
       req.params.id,
       req.body,
       { new: true }
@@ -48,7 +48,7 @@ export const updateArticle = async (req, res) => {
 
 export const deleteArticle = async (req, res) => {
   
-    await CreateArticle.findByIdAndDelete(req.params.id);
+    await Article.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: 'Article deleted successfully' });
   
     res.status(500).json({ message: 'Server error'});
