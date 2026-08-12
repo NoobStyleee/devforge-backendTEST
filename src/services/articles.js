56567f9534cbd503c896c9937a192caa3f7cbb33
Drapak Dmytro <dd.study05@gmail.com>
2026-08-12 14:28:14 +0300

    fix

--- src/services/articles.js
+++
@@ -1,8 +0,0 @@
-import { Article } from '../models/Article.js';
-
-export const findArticleById = (id) => Article.findById(id);
-
-export const updateArticleById = (id, data) =>
-  Article.findByIdAndUpdate(id, data, { new: true });
-
-export const deleteArticleById = (id) => Article.findByIdAndDelete(id);
