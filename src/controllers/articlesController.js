9ccc3dfefd5a3688d2609cbf79a405a056e5aaf9
Drapak Dmytro <dd.study05@gmail.com>
2026-08-13 10:49:43 +0300

    fix

--- src/controllers/articlesController.js
+++ src/controllers/articlesController.js
@@ -41,22 +41,40 @@ export const getArticleByIdController = async (req, res) => {
   res.status(200).json(article);
 };
 
+export const createArticle = async (req, res) => {
+  const { title, desc, img, date, author } = req.body;
+  const ownerId = req.user._id;
 
-export const createArticle = async (req, res) => {    
-    const { title, desc, img, date, author } = req.body;
-    const ownerId = req.user._id; 
+  const newArticle = await Article.create({
+    title,
+    desc,
+    img,
+    ownerId,
+    date,
+    author,
+  });
 
-    const newArticle = await Article.create({
-      title,
-      desc,
-      img,   
-      ownerId,
-      date,
-      author, 
-    });
-  
   if (!newArticle) {
     throw createHttpError();
   }
-    res.status(201).json(newArticle);        
+  res.status(201).json(newArticle);
+};
+
+export const deleteArticle = async (req, res) => {
+  const { id } = req.params;
+  const { _id: userId } = req.user;
+
+  const article = await Article.findById(id);
+
+  if (!article) {
+    throw createHttpError(404, 'Article not found');
+  }
+
+  if (article.ownerId.toString() !== userId.toString()) {
+    throw createHttpError(403, 'You can delete only your own articles');
+  }
+
+  await Article.findByIdAndDelete(id);
+
+  res.status(200).json({ message: 'Article deleted successfully' });
 };
