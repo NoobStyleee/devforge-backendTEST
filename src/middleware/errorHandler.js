b279075a6b40b1647e340147494943b048f0067d
VolodymyrB <volodymyrbedryk@gmail.com>
2026-08-16 16:11:52 +0200

    feat: added lowerCase

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,10 +2,12 @@ import HttpError from 'http-errors';
 
 export const errorHandler = (error, req, res, next) => {
   if (error instanceof HttpError) {
-    res.status(error.status).json({ message: error.message || error.name });
+    return res
+      .status(error.status)
+      .json({ message: error.message || error.name });
   }
 
-  res.status(500).json({
+  return res.status(500).json({
     message: `${error.message}`,
   });
 };
