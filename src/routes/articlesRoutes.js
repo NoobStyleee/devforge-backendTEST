80baae92984a76cc9329a7e2696e46df53b92827
NoobStyle <mikledanse@mail.ru>
2026-08-18 19:52:36 +0200

    made the img upload in My Profile to be on Cloud

--- src/routes/articlesRoutes.js
+++ src/routes/articlesRoutes.js
@@ -17,13 +17,9 @@ import {
   deleteArticleSchema,
 } from '../validations/articlesValidation.js';
 import { authenticate } from '../middleware/authenticate.js';
-import multer from 'multer';
+import { upload } from '../middleware/upload.js';
 
 const router = Router();
-const upload = multer({
-  dest: 'uploads/',
-  limits: { fileSize: 1024 * 1024 }, // максимум 1Mb
-});
 
 router.get('/articles', celebrate(getArticlesSchema), getArticlesController);
 
